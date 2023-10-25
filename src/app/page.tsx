e146ac3efc7db7893d2d10711f6d6f304a25a0a3
Thorne <ExistentialEnso@users.noreply.github.com>
2023-10-25 16:34:35 -0400

    Copy tweak

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
         <Heading size={2}>👩🏻‍💻 software engineer &amp; tech lead</Heading>
 
         <p className="mb-6">
-            As an eight-year-old in the 90s, I started learning how to program C++, quickly also moving onto building websites &em; back when JavaScript was a total nightmare. Since then, I have earned well over a decade of professional tech experience, including three years as Chief Technology Officer of a Y Combinator-backed startup, CrowdMed.
+            As an eight-year-old in the 90s, I started learning how to program C++, quickly also moving onto building websites, back when JavaScript was a total nightmare. Since then, I have earned well over a decade of professional tech experience, including three years as Chief Technology Officer of a Y Combinator-backed startup, CrowdMed.
         </p>
 
         <p className="mb-6">
