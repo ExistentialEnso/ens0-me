98521a7fb78215fc7fbf9610c9eaf300a5052f67
Thorne <ExistentialEnso@users.noreply.github.com>
2023-10-30 01:53:09 -0400

    Button feedback during submit

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -18,12 +18,17 @@ type ButtonProps = {
     children?: React.ReactNode,
     size?: 'sm' | 'md' | 'lg',
     onClick?: (e: React.MouseEvent) => void,
+    disabled?: boolean
 }
 
 const Button = (props: ButtonProps) => {
     const { children, onClick } = props
 
     let className = "bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 px-4 rounded-full"
+
+    if(props.disabled) {
+        className = "bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full"
+    }
     
     const size = props.size ?? "lg"
 
