7764716cb8c6dc117d4ec7620cfc4dc26bb266ea
Thorne <ExistentialEnso@users.noreply.github.com>
2023-11-01 19:50:58 -0400

    Fix for gallery working locally!

--- src/app/nfts/NFTGallery.tsx
+++ src/app/nfts/NFTGallery.tsx
@@ -5,7 +5,7 @@ import TileGalleryView from './TileGalleryView'
 export const revalidate = 60 * 5 // 5 minutes
 
 const NFTGallery = async () => {
-    const nftCategories = await model.NFTCategory.find({}, {name: 1, description: 1, nfts: 1}).populate({path: 'nfts'}).sort({ordinal: 1})
+    const nftCategories = await model.NFTCategory.find({}, {name: 1, description: 1, nfts: 1}).populate({path: 'nfts'}).lean().sort({ordinal: 1})
 
     return(
         <div>
