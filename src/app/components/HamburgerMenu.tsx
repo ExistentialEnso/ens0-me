f79cf433820a2bdc931d3eeb7e61cd820a38e905
Thorne <ExistentialEnso@users.noreply.github.com>
2023-11-01 21:07:47 -0400

    Less reliant on inline styles

--- src/app/components/HamburgerMenu.tsx
+++ src/app/components/HamburgerMenu.tsx
@@ -19,12 +19,7 @@ const HamburgerMenu = () => {
     const [open, setOpen] = useState(false)
 
     let menuStyle = {
-        display: open ? "block" : "none",
-        position: "absolute",
-        left: 0,
-        top: "76px",
-        width: "240px",
-        height: "100%"
+        display: open ? "block" : "none"
     }
 
     return(
@@ -34,8 +29,8 @@ const HamburgerMenu = () => {
             </div>
 
             <Link href="/" className="ml-4 text-2xl text-white align-middle font-bold">Thorne</Link>
-
-            <motion.div animate={open ? 'open' : 'closed'} className="px-5 py-3 bg-slate-100 dark:bg-slate-700 mt-[-4px]" style={menuStyle} variants={menuVariants}>
+            
+            <motion.div animate={open ? 'open' : 'closed'} className="absolute left-0 top-[76px] w-[240px] h-[100%] px-5 py-3 bg-slate-100 dark:bg-slate-700 mt-[-4px]" style={menuStyle} variants={menuVariants}>
                 <Link onClick={() => setOpen(false)} href="/code" className="my-8 block text-xl text-slate-100 hover:text-slate-300">code</Link>
                 <Link onClick={() => setOpen(false)} href="/gaming" className="my-8 block text-xl text-slate-100 hover:text-slate-300">gaming</Link>
                 <Link onClick={() => setOpen(false)} href="/contact" className="my-8 block text-xl text-slate-100 hover:text-slate-300s">contact</Link>
