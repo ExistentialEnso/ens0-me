b22a20d368e8abd1a4d1d62c13491e4dd6c4e0e9
Thorne <ExistentialEnso@users.noreply.github.com>
2024-05-30 00:54:20 -0400

    "coding" instead of "code" in header links

--- src/app/components/HeaderBar.tsx
+++ src/app/components/HeaderBar.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import Link from 'next/link'
 
 /**
- * Component href display header links at the hrefp of the page.
+ * Component for displaying links at the top of the page
  */
 const HeaderBar = () => {
     return(
         <div className="w-full p-5 bg-violet-800 hidden lg:block">
             <Link href="/" className="text-2xl lg:text-2xl font-bold text-white hover:text-violet-200 ml-6">Thorne</Link>
-            <Link href="/code" className="text-lg lg:text-lg text-white hover:text-violet-400 ml-6">code</Link>
+            <Link href="/code" className="text-lg lg:text-lg text-white hover:text-violet-400 ml-6">coding</Link>
             <Link href="/gaming" className="text-lg lg:text-lg text-white hover:text-violet-400 ml-6">gaming</Link>
             <Link href="/contact" className="text-lg lg:text-lg text-white hover:text-violet-400 ml-6">contact</Link>
         </div>
