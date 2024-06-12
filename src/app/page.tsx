eae8b36eb4a2c9729578223138c3c77892507171
Thorne <ExistentialEnso@users.noreply.github.com>
2024-06-12 18:29:31 -0400

    Remove link to NFT page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,12 +67,14 @@ export default function Home() {
         <p className="mb-12">
             Since a young age, I have also had a love of writing, both fiction and non. In 2017, I was <Link href="https://www.nytimes.com/2017/11/02/opinion/twitter-transgender-harassment-problem.html">featured in the New York Times</Link> talking about anti-trans harassment on Twitter. As part of smol farm, I am the lead writer of the <Link href="https://smol.news">smol farm gazette</Link>, a Substack which discusses everything from the technology to small-scale agriculture.
         </p>
+    </main>
+  )
+}
 
-        <Heading size={2}>🌸 more</Heading>
+/*
+ * <Heading size={2}>🌸 more</Heading>
 
         <ul className="list-disc ml-4">
             <li><Link href="/nfts">NFT Collection</Link> - I love collecting digital art and built a gallery of featured pieces of my permanent collection.</li>
         </ul>
-    </main>
-  )
-}
+ */
