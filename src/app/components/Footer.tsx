830ce18ebd48e75ec0d3117fc8380fb595037e50
Thorne <ExistentialEnso@users.noreply.github.com>
2024-06-23 22:46:55 -0400

    Simplify socials

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -11,11 +11,8 @@ const Footer = () => {
         { url: 'https://github.com/ExistentialEnso', label: 'GitHub' },
         { url: 'https://www.x.com/existentialenso', label: 'X' },
         { url: 'https://bsky.app/profile/ens0.me', label: 'BlueSky' },
-        { url: 'https://leetcode.com/ens0/', label: 'LeetCode' },
         { url: 'https://www.reddit.com/u/existentialenso/', label: 'Reddit' },
         { url: 'https://linkedin.com/in/ens0', label: 'LinkedIn' },
-        { url: 'https://www.twitch.tv/ens0plays', label: 'Twitch' },
-        { url: 'https://discord.com/users/248659552654852097', label: 'Discord' },
     ]
 
     return (
